323f6d103325c31cbeeb911620c81f05b5e3a815
Seoncheol Park <pscstat@gmail.com>
2017-08-18 18:10:25 +0900

    Upload examples in `qtree` function

--- R/qtree.R
+++ R/qtree.R
@@ -3,6 +3,17 @@
 #' Ordinary regression trees use the mean squared error loss function. The qtree function uses the tilted absolute value loss function (a specific case is the LAD trees) with the same speed as that of regular trees.
 #'
 #' @return A class outtree
+#' @examples
+#' set.seed(1)
+#' library('tree')
+#' x1 <- c(2,4,3,7,8)
+#' x1 <- x1 + rnorm(5)
+#' x2 <- c(12,19,17,3,5)
+#' x2 <- x2 + rnorm(5)
+#' y <- c(-3, 2, 0, 1, 12)
+#' y <- y + rnorm(5)
+#' data.simple <- data.frame(x1,x2,y)
+#' mod.data <- qtree(y ~ ., data = data.simple, mindev = 0.01, tau = 0.5, minsize = 2, mincut = 1)
 #' @useDynLib qrandomForest, .registration = TRUE
 #' @importFrom stats model.extract na.pass
 #' @exportPattern ^[[:alpha:]]+
